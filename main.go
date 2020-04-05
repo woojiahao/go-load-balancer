ea1916a0d8d4944396cd1fed2adcde420f8335e3
woojiahao <woojiahao1234@gmail.com>
2020-04-05 21:44:47 +0800

    add: backend alive behavior

--- main.go
+++ main.go
@@ -39,3 +39,16 @@ func (s *ServerPool) GetNextPeer() *Backend {
   }
   return nil
 }
+
+func (b *Backend) SetAlive(alive bool) {
+  b.mux.Lock()
+  b.Alive = alive
+  b.mux.Unlock()
+}
+
+func (b *Backend) IsAlive() (alive bool) {
+  b.mux.RLock()
+  alive = b.Alive
+  b.mux.RUnlock()
+  return
+}
